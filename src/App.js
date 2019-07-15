40c7f97ea5eb40b70058b6836757e39b3d2e8f02
Patrick Geiger <patrickjgeiger@gmail.com>
2019-07-15 04:21:43 -0400

    working through course

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ListContacts from "./ListContacts";
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "karen_isgrigg",
+        avatarURL: "http://localhost:5001/karen.jpg"
+      },
+      {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "richardkalehoff",
+        avatarURL: "http://localhost:5001/richard.jpg"
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "tylermcginnis",
+        avatarURL: "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  };
+
+  removeContact = contactToBeDeleted => {
+    this.setState(currenState => {
+      return currenState.contacts.filter(contact => {
+        return contact.id !== contactToBeDeleted.id;
+      });
+    });
+  };
+
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
+        />
       </div>
     );
   }
