d3d73a7ee54cd818fd4e705bae948709b00d78d8
Patrick Geiger <patrickjgeiger@gmail.com>
2019-07-15 19:04:08 -0400

    Revert "working through course"
    This reverts commit 40c7f97ea5eb40b70058b6836757e39b3d2e8f02.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
-import React, { Component } from "react";
-import ListContacts from "./ListContacts";
+import React, { Component } from 'react';
 
 class App extends Component {
-  state = {
-    contacts: [
-      {
-        id: "karen",
-        name: "Karen Isgrigg",
-        handle: "karen_isgrigg",
-        avatarURL: "http://localhost:5001/karen.jpg"
-      },
-      {
-        id: "richard",
-        name: "Richard Kalehoff",
-        handle: "richardkalehoff",
-        avatarURL: "http://localhost:5001/richard.jpg"
-      },
-      {
-        id: "tyler",
-        name: "Tyler McGinnis",
-        handle: "tylermcginnis",
-        avatarURL: "http://localhost:5001/tyler.jpg"
-      }
-    ]
-  };
-
-  removeContact = contactToBeDeleted => {
-    this.setState(currenState => {
-      return currenState.contacts.filter(contact => {
-        return contact.id !== contactToBeDeleted.id;
-      });
-    });
-  };
-
   render() {
     return (
       <div>
-        <ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-        />
+        Hello World
       </div>
     );
   }
